8d0735d825c4839273406dd5b07f3bddc0ec2711
Ilya Yaroshenko <ilyayaroshenko@gmail.com>
2016-04-12 13:41:22 +0200

    init

--- source/mir/sum.d
+++ source/mir/sum.d
@@ -7,7 +7,7 @@ Authors: Ilya Yaroshenko
 
 Source: $(PHOBOSSRC std/experimental/las/_sum.d)
 */
-module mir.las.sum;
+module mir.sum;
 
 ///
 unittest
@@ -2130,7 +2130,7 @@ private template sumType(Range)
     alias sumType = typeof(T.init + T.init);
 }
 
-package(mir.las):
+package:
 
 template isSummable(F)
 {
