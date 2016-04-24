b01a1f96be84e4ad9a3a273749ba508b25b09ca0
Ilya Yaroshenko <ilyayaroshenko@gmail.com>
2016-04-24 17:39:54 +0200

    add math doc module

--- source/mir/math.d
+++ source/mir/math.d
@@ -73,7 +73,7 @@ unittest
 }
 
 /++
-Natural logarithms of minus Euler–Mascheroni constant.
+Exponent of minus Euler–Mascheroni constant.
 +/
 enum real expMEuler = 0x0.8fbbcf07f2e5f2c56894d7014c3086p0;
 
