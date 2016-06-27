a22b909126194c76213b2fb7b97fc40a77078ca7
Sebastian Wilzbach <seb@wilzba.ch>
2016-06-27 02:59:54 +0200

    cleanup plotting code

--- source/mir/random/tinflex/internal/plot.d
+++ source/mir/random/tinflex/internal/plot.d
@@ -9,7 +9,10 @@ import mir.random.tinflex : Tinflex;
 version(tinflex_plot):
 
 /**
-Generates a series of y-values that can be used for plotting.
+Generates a series of y-values of all hat or squeeze functions of an Tinflex
+object. This is useful for plotting.
+Points in xs need to be in the boundaries of the Tinflex algorithm, otherwise they
+will be ignored.
 
 Params:
     t = Tinflex generator
@@ -18,34 +21,33 @@ Params:
 */
 auto plot(F0, S)(Tinflex!(F0, S) t, S[] xs, bool hat = true)
 {
+    import mir.random.tinflex.internal.transformations : inverse;
     import std.algorithm.comparison : clamp;
+
     S[] ys = new S[xs.length];
     int k = 0;
     S rMin = xs[0];
     S rMax = xs[$ - 1];
+
+    // each interval is defined in clear bounds
+    // as we iterate over the points to be plotted, we have to check to use the
+    // correct hat/squeeze function for the current point
     outer: foreach (i, v; t.gps)
     {
+        // calculate bounds of the current interval
         S l = clamp(v.x, rMin, rMax);
-        S r;
-        if (i < t.gps.length - 1)
-        {
-            r = clamp(t.gps[i + 1].x, rMin, rMax);
-        }
-        else
-        {
-            r = rMax;
-        }
+        // ignore unmatched points at the left
+        while (xs[k] < l)
+            k++;
+
+        S r = (i < t.gps.length - 1) ? clamp(t.gps[i + 1].x, rMin, rMax) : rMax;
+
+        // until the right bound is reached
         while (xs[k] < r)
         {
-            if (hat)
-                ys[k] = v.hat(xs[k]);
-            else
-                ys[k] = v.squeeze(xs[k]);
-
-            import mir.random.tinflex.internal.transformations : inverse;
-            ys[k] = inverse(ys[k], v.c);
-            k++;
-            if (k >= ys.length)
+            // reverse our T_c transformation and calculate the value
+            ys[k] = inverse((hat) ? v.hat(xs[k]) : v.squeeze(xs[k]), v.c);
+            if (k++ >= ys.length)
                 break outer;
         }
     }
