fce225d1e93a2ca589f411fa41e76f5cfc310368
Sebastian Wilzbach <seb@wilzba.ch>
2016-07-01 15:37:15 +0200

    refactor plotting to make it more generic

--- examples/tinflex_plot.d
+++ examples/tinflex_plot.d
@@ -21,31 +21,6 @@ shared static this() {
     py_init();
 }
 
-/**
-Convenience method to sample Arrays with sample r
-This will be replaced with a more sophisticated version in later versions.
-
-Params:
-    r = random sampler
-    n = number of times to sample
-Returns: Randomly sampled Array of length n
-*/
-typeof(R.init())[] sample(R, RNG)(R r, int n, RNG rnd)
-{
-    alias S = typeof(r());
-    S[] arr = new S[n];
-    foreach (ref s; arr)
-        s = r(rnd);
-    return arr;
-}
-
-/// ditto
-typeof(R.init())[] sample(R)(R r, int n)
-{
-    import std.random : rndGen;
-    return sample(r, n, rndGen);
-}
-
 // plot histogram with matplotlib
 void npPlotHistogram(S)(S[] values, string fileName)
 {
@@ -84,7 +59,8 @@ Params:
     isHat = whether hat (true) or squeeze (false) should be plotted
     isTransformed = whether to plot the transformed functions
 */
-auto plotArea(S, T)(in TinflexInterval!S[] intervals, T[] xs, bool isHat = true, bool isTransformed = false)
+auto plotArea(S, T)(in TinflexInterval!S[] intervals, T[] xs, bool isHat = true,
+                    bool isTransformed = false)
 {
     import mir.random.tinflex : inverse;
     import std.algorithm.comparison : clamp;
@@ -121,9 +97,11 @@ auto plotArea(S, T)(in TinflexInterval!S[] intervals, T[] xs, bool isHat = true,
     return ys;
 }
 
-auto npPlotHatAndSqueezeArea(S, Pdf)(in TinflexInterval!S[] intervals, Pdf pdf, string fileName,
-                              S stepSize = 0.1, S left = -3, S right = 3)
+auto npPlotHatAndSqueezeArea(S, Pdf)(in TinflexInterval!S[] intervals, Pdf pdf,
+                                     string fileName, S stepSize = 0.1,
+                                     S left = -3, S right = 3)
 {
+    import std.algorithm.comparison : max, min;
     import std.array : array;
     import std.traits : ReturnType;
 
@@ -138,7 +116,11 @@ auto npPlotHatAndSqueezeArea(S, Pdf)(in TinflexInterval!S[] intervals, Pdf pdf, 
 
     auto pythonContext = new InterpContext();
     alias T = double;
-    T[] xs = iota!(T, T, T)(left, right + stepSize, stepSize).array;
+
+    S l = max(left, intervals[0].lx);
+    S r = min(right, intervals[$ - 1].rx);
+
+    T[] xs = iota!(T, T, T)(l, r + stepSize, stepSize).array;
     pythonContext.xs = xs.toNumpyArray;
 
     // PDF
@@ -165,11 +147,19 @@ auto npPlotHatAndSqueezeArea(S, Pdf)(in TinflexInterval!S[] intervals, Pdf pdf, 
 /**
 Plots every interval as a separate line with a given stepsize.
 */
-auto plotWithIntervals(S)(in TinflexInterval!S[] intervals, bool isHat = true, S stepSize = 0.01, bool isTransformed = false)
+auto plotWithIntervals(S)(in TinflexInterval!S[] intervals, bool isHat = true,
+                          S stepSize = 0.01, bool isTransformed = false, S left = -3,
+                          S right = 3)
+in
+{
+    assert(intervals[0].lx <= left);
+    assert(right <= intervals[$ - 1].rx);
+}
+body
 {
     import mir.random.tinflex : inverse;
+    import std.algorithm.comparison : max, min;
     import std.math : ceil;
-    import std.algorithm.comparison : max;
 
     auto len = intervals.length;
 
@@ -182,7 +172,7 @@ auto plotWithIntervals(S)(in TinflexInterval!S[] intervals, bool isHat = true, S
         size_t nrIntervals = cast(size_t) ((iv.rx - iv.lx) / stepSize).ceil;
         xs[i] = new S[max(nrIntervals, 2)];
         ys[i] = new S[max(nrIntervals, 2)];
-        S x = iv.lx;
+        S x = max(iv.lx, left);
         foreach (k; 0..nrIntervals)
         {
             xs[i][k] = x;
@@ -195,17 +185,22 @@ auto plotWithIntervals(S)(in TinflexInterval!S[] intervals, bool isHat = true, S
         if (x < iv.rx || nrIntervals == 1)
         {
             // plot last value too
-            xs[i][$ - 1] = iv.rx;
-            ys[i][$ - 1] = (isHat) ? iv.hat(iv.rx) : iv.squeeze(iv.rx);
+            S r = iv.rx;
+            xs[i][$ - 1] = min(iv.rx, right);
+            ys[i][$ - 1] = (isHat) ? iv.hat(r) : iv.squeeze(r);
         }
     }
     import std.typecons : tuple;
     return tuple!("xs", "ys")(xs, ys);
 }
 
-auto npPlotHatAndSqueeze(S, Pdf)(in TinflexInterval!S[] intervals, Pdf pdf, string fileName,
-                              S stepSize = 0.1, S left = -3, S right = 3)
+auto npPlotHatAndSqueeze(S, Pdf)(in TinflexInterval!S[] intervals, Pdf pdf,
+                                 string fileName, S stepSize = 0.01,
+                                 S left = -3, S right = 3)
 {
+    import std.algorithm.comparison : max, min;
+    import std.array : array;
+
     static immutable script = `
         import matplotlib.pyplot as plt
         plt.plot(xs, ys, color='black')
@@ -219,18 +214,22 @@ auto npPlotHatAndSqueeze(S, Pdf)(in TinflexInterval!S[] intervals, Pdf pdf, stri
 
     auto pythonContext = new InterpContext();
     alias T = double;
-    import std.array : array;
-    T[] xs = iota!(T, T, T)(left, right + stepSize, stepSize).array;
-    auto hats = plotWithIntervals(intervals, true);
-    // TODO: this allocates the xs array twice
-    auto squeezes = plotWithIntervals(intervals, false);
 
+    S l = max(left, intervals[0].lx);
+    S r = min(right, intervals[$ - 1].rx);
+
+    // PDF
+    T[] xs = iota!(T, T, T)(l, r + stepSize, stepSize).array;
     T[] ys = new T[xs.length];
     foreach (i, ref y; ys)
         y = pdf(xs[i]);
 
     pythonContext.xs = xs.toNumpyArray;
     pythonContext.ys = ys.toNumpyArray;
+
+    auto hats = plotWithIntervals(intervals, true, stepSize, false, l, r);
+    // TODO: this allocates the xs array twice
+    auto squeezes = plotWithIntervals(intervals, false, stepSize, false, l, r);
 
     // TODO: do something smart to convert hat.xs to list of numpy arrays
     pythonContext.xsHS = hats.xs.d_to_python;
@@ -241,157 +240,176 @@ auto npPlotHatAndSqueeze(S, Pdf)(in TinflexInterval!S[] intervals, Pdf pdf, stri
     pythonContext.py_stmts(script);
 }
 
-/**
-Simple plotting
-*/
-void test(F0, S, Pdf)(Tinflex!(F0, S) tf, Pdf f0, string fileName, int left = -3, int right = 3)
+/// Plotting helper
+struct CFlex(S)
 {
-    import std.math : exp;
-    auto pdf = (S x) => exp(f0(x));
-    // first plot hat/squeeze in case we crash during sampling
-    tf.intervals.npPlotHatAndSqueeze(pdf, fileName ~ "_hs.pdf");
-    tf.intervals.npPlotHatAndSqueezeArea(pdf, fileName ~ "_hs_area.pdf");
+    int n;
+    string plotDir;
+    S rho;
+    S stepSize = 0.01;
 
-    import std.random : rndGen;
-    rndGen.seed(42);
-    auto values = tf.sample(2_000, rndGen);
+    /// @@@BUG@@@ template injection doesn't work with opCall
+    auto plot(string name, in S function(S) f0, in S function(S) f1, in S function(S) f2,
+         S c, S[] points, S left = -3, S right = 3) const
+    {
+        auto cs = new S[points.length - 1];
+        foreach (ref d; cs)
+            d = c;
+        return plot(name, f0, f1, f2, cs, points, left, right);
+    }
 
-    values.npPlotHistogram(fileName ~ "_hist.pdf");
+    auto plot(string name, in S function(S) f0, in S function(S) f1, in S function(S) f2,
+         S[] cs, S[] points, S left = -3, S right = 3) const
+    {
+        import std.math : exp;
+        import std.random : Mt19937;
+        auto tf = tinflex(f0, f1, f2, cs, points, rho);
+        auto pdf = (S x) => exp(f0(x));
 
-    // save values to file for further processing
-    //auto f = File(fileName ~ "_values.csv", "w");
-    //f.writeln(values.map!`a.to!string`.joiner(","));
+        string fileName = plotDir.buildPath(name);
+
+        // first plot hat/squeeze in case we crash during sampling
+        tf.intervals.npPlotHatAndSqueeze(pdf, fileName ~ "_hs.pdf",
+            stepSize, left, right);
+        tf.intervals.npPlotHatAndSqueezeArea(pdf, fileName ~ "_hs_area.pdf",
+            stepSize, left, right);
+
+        import std.random : Mt19937;
+        auto gen = Mt19937(42);
+        S[] values = new S[n];
+        foreach (ref v; values)
+            v = tf(gen);
+
+        values.npPlotHistogram(fileName ~ "_hist.pdf");
+
+        // save values to file for further processing
+        //auto f = File(fileName ~ "_values.csv", "w");
+        //f.writeln(values.map!`a.to!string`.joiner(","));
+        return true;
+    }
 }
 
-void test0(string folderName)
+void test0(S, F)(in ref F test)
 {
-    auto f0 = (double x) => -x^^4 + 5 * x^^2 - 4;
-    auto f1 = (double x) => 10 * x - 4 * x ^^ 3;
-    auto f2 = (double x) => 10 - 12 * x ^^ 2;
-    auto tf = tinflex(f0, f1, f2, 1.5, [-3.0, -1.5, 0.0, 1.5, 3]);
-    tf.test(f0, folderName.buildPath("dist0"));
+    auto f0 = (S x) => -x^^4 + 5 * x^^2 - 4;
+    auto f1 = (S x) => 10 * x - 4 * x ^^ 3;
+    auto f2 = (S x) => 10 - 12 * x ^^ 2;
+    test.plot("dist0", f0, f1, f2, 1.5, [-3.0, -1.5, 0.0, 1.5, 3]);
 }
 
 // default tinflex testing distribution
-void test1(string folderName)
+void test1(S, F)(in ref F test)
 {
-    auto f0 = (double x) => -x^^4 + 5 * x^^2 - 4;
-    auto f1 = (double x) => 10 * x - 4 * x ^^ 3;
-    auto f2 = (double x) => 10 - 12 * x ^^ 2;
-    auto tinflex = (double c,  double[] ips) => tinflex(f0, f1, f2, c, ips);
-    import std.conv : to;
+    auto f0 = (S x) => -x^^4 + 5 * x^^2 - 4;
+    auto f1 = (S x) => 10 * x - 4 * x ^^ 3;
+    auto f2 = (S x) => 10 - 12 * x ^^ 2;
 
     foreach (c; [0.1, 0.5, 1])
     {
-        tinflex(c, [-3.0, -1.5, 0.0, 1.5, 3]).test(f0, folderName.buildPath("dist1_a" ~ c.to!string));
+        test.plot("dist1_a" ~ c.to!string, f0, f1, f2, c, [-3.0, -1.5, 0.0, 1.5, 3]);
     }
 
     foreach (c; [-0.9, -0.5, -0.2, 0])
     {
-        tinflex(c, [-double.infinity, -2.1, -1.05, 0.1, 1.2, 2, double.infinity]).test(f0, folderName.buildPath("dist1_b" ~ c.to!string));
-        tinflex(c, [-double.infinity, -1, 0, 1, double.infinity]).test(f0, folderName.buildPath("dist1_c" ~ c.to!string));
-        tinflex(c, [-2, 0, 1.5]).test(f0, folderName.buildPath("dist1_d" ~ c.to!string), -4, 6);
+        test.plot("dist1_b" ~ c.to!string, f0, f1, f2, c,
+            [-S.infinity, -2.1, -1.05, 0.1, 1.2, 2, S.infinity]);
+        test.plot("dist1_c" ~ c.to!string, f0, f1, f2, c, [-S.infinity, -1, 0, 1, S.infinity]);
+        test.plot("dist1_d" ~ c.to!string, f0, f1, f2, c, [-2, 0, 1.5], -4, 6);
     }
 
     foreach (c; [-2, -1.5, -1])
     {
-        tinflex(c, [-3.0, -2.1, -1.05, 0.1, 1.2, 3]).test(f0, folderName.buildPath("dist1_e" ~ c.to!string));
+        test.plot("dist1_e" ~ c.to!string, f0, f1, f2, c, [-3.0, -2.1, -1.05, 0.1, 1.2, 3]);
     }
 }
 
 // test at and near extrema
-void test2(string folderName)
+void test2(S, F)(in ref F test)
 {
-    auto f0 = (double x) => -2 *  x^^4 + 4 * x^^2;
-    auto f1 = (double x) => -8 *  x^^3 + 8 * x;
-    auto f2 = (double x) => -24 * x^^2 + 8;
-    auto tinflex = (double c,  double[] ips) => tinflex(f0, f1, f2, c, ips);
-
-    import std.conv : to;
+    auto f0 = (S x) => -2 *  x^^4 + 4 * x^^2;
+    auto f1 = (S x) => -8 *  x^^3 + 8 * x;
+    auto f2 = (S x) => -24 * x^^2 + 8;
 
     foreach (c; [-2, -1.1, -1, 0.5, 1, 1.5, 2])
     {
-        tinflex(c, [-3, -1, 0, 1, 3]).test(f0, folderName.buildPath("dist2_c_" ~ c.to!string));
-        tinflex(c, [-3, -1 + (cast(real) 2) ^^-52, 1e-20, 1 - (cast(real) 2)^^(-53), 3]).test(f0, folderName.buildPath("dist2_d_" ~ c.to!string));
+        test.plot("dist2_c_" ~ c.to!string, f0, f1, f2, c, [-3, -1, 0, 1, 3]);
+        test.plot("dist2_d_" ~ c.to!string, f0, f1, f2, c,
+            [-3, -1 + (cast(S) 2) ^^-52, 1e-20, 1 - (cast(S) 2)^^(-53), 3]);
     }
 
     foreach (c; [-0.9, -0.5, -0.2, 0])
     {
-        tinflex(c, [-double.infinity, -2, -1, 0, 1, 2, double.infinity]).test(f0, folderName.buildPath("dist2_a" ~ c.to!string));
-        tinflex(c, [-double.infinity, -2, -1 + (cast(real)2)^^-52, 1e-20, 1-(cast(real)2)^^(-53), 2, double.infinity]).test(f0, folderName.buildPath("dist2_" ~ c.to!string));
+        test.plot("dist2_a" ~ c.to!string, f0, f1, f2, c,
+            [-S.infinity, -2, -1, 0, 1, 2, S.infinity]);
+        test.plot("dist2_" ~ c.to!string, f0, f1, f2, c,
+            [-S.infinity, -2, -1 + (cast(S)2)^^-52, 1e-20, 1-(cast(S)2)^^(-53), 2, S.infinity]);
     }
 }
 
 // density vanishes at boundaries
-void test3(string folderName)
+void test3(S, F)(in ref F test)
 {
     import std.math : log;
-    auto f0 = (double x) => log(1 - x^^4);
-    auto f1 = (double x) => -4 * x^^3 / (1 - x^^4);
-    auto f2 = (double x) => -(4 * x^^6 + 12 * x^^2) / (x^^8 - 2 * x^^4 + 1);
-    auto tinflex = (double c,  double[] ips) => tinflex(f0, f1, f2, c, ips, 1.01);
+    auto f0 = (S x) => cast(S) log(1 - x^^4);
+    auto f1 = (S x) => -4 * x^^3 / (1 - x^^4);
+    auto f2 = (S x) => -(4 * x^^6 + 12 * x^^2) / (x^^8 - 2 * x^^4 + 1);
+    auto tinflex = (S c,  S [] ips) => tinflex(f0, f1, f2, c, ips, 1.01);
 
-    import std.conv : to;
     foreach (c; [1.5, 2])
-        tinflex(c, [-1, -0.9, -0.5, 0.5, 0.9, 1])
-        .test(f0, folderName.buildPath("dist3_a_" ~ c.to!string), -1, 1);
+        test.plot("dist3_a_" ~ c.to!string, f0, f1, f2, c, [-1, -0.9, -0.5, 0.5, 0.9, 1], -1, 1);
 
     foreach (c; [-2, -1.5, -1, -0.9,  -0.5, -0.2, 0, 0.1, 0.5, 1])
-        tinflex(c, [-1, -0.5, 0.5, 1]).test(f0, folderName.buildPath("dist3_b_" ~ c.to!string));
+        test.plot("dist3_b_" ~ c.to!string, f0, f1, f2, c, [-1, -0.5, 0.5, 1]);
 }
 
 // density with pole
-void test4(string folderName)
+void test4(S, F)(in ref F test)
 {
     import std.math : abs, log;
-    auto f0 = (real x) => - log(abs(x)) / 2;
-    auto f1 = (real x) => -1 / (2 * x);
-    auto f2 = (real x) => 1 / (2 * x^^2);
+    auto f0 = (S x) => - cast(S) log(abs(x)) * S(0.5);
+    auto f1 = (S x) => -1 / (2 * x);
+    auto f2 = (S x) => 1 / (2 * x * x);
 
-    tinflex(f0, f1, f2, 1.5, [-1.0, 0, 1]).test(f0, folderName.buildPath("dist4"));
+    test.plot("dist4", f0, f1, f2, 1.5, [-1.0, 0, 1]);
 }
 
 // different values for c
-void test5(string folderName)
+void test5(S, F)(in ref F test)
 {
-    auto f0 = (double x) => -2 * x^^4 + 4 * x^^2;
-    auto f1 = (double x) => -8 * x^^3 + 8 * x;
-    auto f2 = (double x) => -24 * x^^2 + 8;
+    auto f0 = (S x) => -2 * x^^4 + 4 * x^^2;
+    auto f1 = (S x) => -8 * x^^3 + 8 * x;
+    auto f2 = (S x) => -24 * x^^2 + 8;
 
-    tinflex(f0, f1, f2, [-0.5, 2, -2, 0.5, -1, 0], [-double.infinity, -2, -1, 0, 1, 2, double.infinity])
-    .test(f0, folderName.buildPath("dist5_b"));
+    test.plot("dist5_a", f0, f1, f2, [-0.5, 2, -2, 0.5, -1, 0],
+        [-S.infinity, -2, -1, 0, 1, 2, S.infinity]);
 
-    tinflex(f0, f1, f2, [-0.5, 2, -2, 0.5, -1, 0], [-3, -2, -1, 0, 1, 2, 3])
-    .test(f0, folderName.buildPath("dist5_b"));
+    test.plot("dist5_b", f0, f1, f2, [-0.5, 2, -2, 0.5, -1, 0], [-3, -2, -1, 0, 1, 2, 3]);
 }
 
 // inflection point at boundary
-void test6(string folderName)
+void test6(S, F)(in ref F test)
 {
-    auto f0 = (double x) => -x^^4 + 6 * x^^2;
-    auto f1 = (double x) => 12 * x - 4 * x^^3;
-    auto f2 = (double x) => 12 - 12 * x^^2;
+    auto f0 = (S x) => -x^^4 + 6 * x^^2;
+    auto f1 = (S x) => 12 * x - 4 * x^^3;
+    auto f2 = (S x) => 12 - 12 * x^^2;
 
-    tinflex(f0, f1, f2, 0, [-double.infinity, -2, -1, 0, 1, 2, double.infinity])
-    .test(f0, folderName.buildPath("dist6"));
+    test.plot("dist6", f0, f1, f2, 0, [-S.infinity, -2, -1, 0, 1, 2, S.infinity]);
 }
 
 
-void test_normal(string folderName)
+void test_normal(S, F)(in ref F test)
 {
     import std.math : exp, log, PI, sqrt;
-    alias S = real;
     //S[] points = [-S.infinity, -1.5, 0, 1.5, S.infinity];
     S[] points = [-3, -1.5, 0, 1.5, 3];
-    S sqrt2PI = sqrt(2 * PI);
+    enum S sqrt2PI = sqrt(2 * PI);
     //auto f0 = (S x) => 1 / (exp(x * x / 2) * sqrt2PI);
     //auto f1 = (S x) => -(x/(exp(x * x/2) * sqrt2PI));
     //auto f2 = (S x) => (-1 + x * x) / (exp(x * x/2) * sqrt2PI);
-    auto f0 = (S x) => log(1 / (exp(x * x / 2) * sqrt2PI));
+    auto f0 = (S x) => cast(S) log(1 / (exp(x * x / 2) * sqrt2PI));
     auto f1 = (S x) => -x;
-    auto f2 = (S x) => -1.0;
-    tinflex(f0, f1, f2, 1.5, points)
-    .test(f0, folderName.buildPath("dist_normal"));
+    auto f2 = (S x) => S(-1);
+    test.plot("dist_normal", f0, f1, f2, 1.5, points);
 }
 
 void main(string[] args)
@@ -399,16 +417,21 @@ void main(string[] args)
     bool runAll = args.length <= 1;
 
     import std.file : exists, mkdir;
-    string folderName = "plots";
+    alias T = double;
+    string plotDir = "plots";
+    int n = 5_000;
+    T rho = 1.1;
 
-    if (!folderName.exists)
-        folderName.mkdir;
+    if (!plotDir.exists)
+        plotDir.mkdir;
 
     import std.meta : AliasSeq;
     import std.traits : fullyQualifiedName;
     import std.algorithm.searching : canFind;
 
     alias funs = AliasSeq!(test0, test1, test2, test3, test4, test5, test6, test_normal);
+
+    auto cf = CFlex!T(n, plotDir, rho);
     foreach (i, f; funs)
     {
         bool isSelected = runAll;
@@ -428,7 +451,7 @@ void main(string[] args)
         if (isSelected)
         {
             writefln("=== Running: %s", funName);
-            f(folderName);
+            f!(T, typeof(cf))(cf);
         }
     }
 }
