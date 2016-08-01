8890d3d9812afe36fc51d82249f0a1e38fde234e
Sebastian Wilzbach <seb@wilzba.ch>
2016-08-01 08:22:43 +0200

    windows test

--- source/mir/zzz_test_windows.d
+++ source/mir/zzz_test_windows.d
@@ -8,7 +8,12 @@ unittest
     writeln("test windows");
     assert(f == f2);
     writeln("test windows II");
-    S f3 = f2 + 0;
+    import std.math;
+    S f3 = 0 + f2 - 0;
+    writefln("up: %a", f2.nextUp);
+    writefln("down: %a", f2.nextDown);
+    writefln("f2: %a", f3);
+    writefln("f3: %a", f3);
     assert(f == f3);
     writeln("test windows III");
 }
