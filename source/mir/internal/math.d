297a3ee0262d8e55c81d5b0d7a13b09107045884
Ilya Yaroshenko <ilyayaroshenko@gmail.com>
2016-11-27 14:00:48 +0200

    fix ci

--- source/mir/internal/math.d
+++ source/mir/internal/math.d
@@ -4,75 +4,75 @@ version(LDC)
 {
     pragma(LDC_no_moduleinfo);
 
-	import ldc.intrinsics;
+    import ldc.intrinsics;
 
-	alias sqrt = llvm_sqrt;
+    alias sqrt = llvm_sqrt;
 
-	alias sin = llvm_sin;
+    alias sin = llvm_sin;
 
-	alias cos = llvm_cos;
+    alias cos = llvm_cos;
 
-	alias powi = llvm_powi;
+    alias powi = llvm_powi;
 
-	alias pow = llvm_powi;
+    alias pow = llvm_powi;
 
-	alias pow = llvm_pow;
+    alias pow = llvm_pow;
 
-	alias exp = llvm_exp;
+    alias exp = llvm_exp;
 
-	alias log = llvm_log;
+    alias log = llvm_log;
 
-	alias fabs = llvm_fabs;
+    alias fabs = llvm_fabs;
 
-	alias floor = llvm_floor;
+    alias floor = llvm_floor;
 
-	alias exp2 = llvm_exp2;
+    alias exp2 = llvm_exp2;
 
-	alias log10 = llvm_log10;
+    alias log10 = llvm_log10;
 
-	alias log2 = llvm_log2;
+    alias log2 = llvm_log2;
 
-	alias ceil = llvm_ceil;
+    alias ceil = llvm_ceil;
 
-	alias trunc = llvm_trunc;
+    alias trunc = llvm_trunc;
 
-	alias rint = llvm_rint;
+    alias rint = llvm_rint;
 
-	alias nearbyint = llvm_nearbyint;
+    alias nearbyint = llvm_nearbyint;
 
-	alias copysign = llvm_copysign;
+    alias copysign = llvm_copysign;
 
-	alias round = llvm_round;
+    alias round = llvm_round;
 
-	alias fmuladd = llvm_fmuladd;
+    alias fmuladd = llvm_fmuladd;
 
-	alias fmin = llvm_minnum;
+    alias fmin = llvm_minnum;
 
-	alias fmax = llvm_maxnum;
+    alias fmax = llvm_maxnum;
 }
 else
 {
-	public import std.math :
-		sqrt,
-		sin,
-		cos,
-		pow,
-		pow,
-		exp,
-		log,
-		fabs,
-		floor,
-		exp2,
-		log10,
-		log2,
-		ceil,
-		trunc,
-		rint,
-		nearbyint,
-		copysign,
-		round,
-		fmin,
-		fmax;
+    public import std.math :
+        sqrt,
+        sin,
+        cos,
+        pow,
+        pow,
+        exp,
+        log,
+        fabs,
+        floor,
+        exp2,
+        log10,
+        log2,
+        ceil,
+        trunc,
+        rint,
+        nearbyint,
+        copysign,
+        round,
+        fmin,
+        fmax;
 
     T fmuladd(T)(const T a, const T b, const T c)
     {
